ded9b85097a53ac51e9815c7d178dfcbc149ad5d
Andre Dragacevac <a.dragacevac@icloud.com>
2022-10-14 12:22:44 -0400

    commit tree 23c1543de4bd59136705714458ee2723b3aa7dac

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    let distance = Math.abs(42-location)
+    return distance
+}
+distanceFromHqInBlocks(43)
+
+function distanceFromHqInFeet(location){
+    let distance = Math.abs(42-location)*264
+    return distance
+}
+distanceFromHqInFeet(43)
+
+function distanceTravelledInFeet(start,destination){
+    let distance = Math.abs(start-destination)*264
+    return distance
+}
+distanceTravelledInFeet()
+
+function calculatesFarePrice(start,destination){
+    let distance = Math.abs((start-destination)*264)
+    let fare = (distance *.02)-(400*.02)
+    let flat = 25
+    if (distance<=400){
+    return /*`Your total is $ ${*/fare*0/*}. Thank you!`*/
+    }
+    else if (distance>400 && distance<2000){
+    return /*`Your total is $ ${*/fare.toFixed(2)/*}. Thank you!`*/
+    }
+    if (distance>2000 && distance<2500){
+        return /*`Your total is $ ${*/flat/*}. Thank you!`*/
+    }
+    else
+    return 'cannot travel that far'
+}
